8b78378e5b7359d9a1fb7ab4aad545c181afb72d
chhartmann <chhartmann@web.de>
2026-02-19 18:29:03 +0100

    Add script to render CV in HTML and PDF formats
    This script builds both HTML and PDF versions of the CV by rendering an R Markdown file and converting the temporary HTML to PDF.

---
+++ render_cv.r
@@ -0,0 +1,16 @@
+# This script builds both the HTML and PDF versions of your CV
+
+# Knit the HTML version
+rmarkdown::render("cv/cv.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "cv/cv.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("cv/cv.rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+#  Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "cv/cv.pdf")
